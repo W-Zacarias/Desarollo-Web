b92859739227c8cfa20b7b9f8a711b67f63ddc48
W-Zacarias <wzacariasl@miumg.edu.gt>
2021-09-10 19:06:38 -0600

    Actividad 2.4

--- Apps/home/admin.py
+++
@@ -1,7 +0,0 @@
-
-from django.contrib import admin
-from .models import Estudiantes, Administradores, Articulos, Comentarios
-admin.site.register(Estudiantes)
-admin.site.register(Administradores)
-admin.site.register(Articulos)
-admin.site.register(Comentarios)
